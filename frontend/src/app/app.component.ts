624ad6c213415d24dbf082a2206a2352adc52d59
Bryana Ojopi Banegas <ojopibryana5@gmail.com>
2022-09-21 19:40:25 -0400

    se agregó el listado de usuarios

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -1,10 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { PersonalService } from "./services/personal.service";
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'frontend';
+export class AppComponent implements OnInit {
+  title = 'Recursos Humanos';
+  personal: any
+
+  constructor( public personalService: PersonalService) {}
+
+  ngOnInit(): void {
+      this.personalService.getPersonal().subscribe(data => {
+        console.log(data)
+        this.personal = data
+      })
+  }
+  
 }
