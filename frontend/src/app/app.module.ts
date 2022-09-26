14c414030470d8d18f548d5c9f3954f3726218ee
Bryana Ojopi Banegas <ojopibryana5@gmail.com>
2022-09-26 01:03:35 -0400

    se agrego el componente de registro de personal

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -7,6 +7,7 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ListadoPersonalComponent } from './pages/personal/listado-personal/listado-personal.component';
 import { CuentagestionComponent } from './pages/cuentagestion/cuentagestion.component';
+import { RegistrarPersonalComponent } from './pages/personal/registrar-personal/registrar-personal.component';
 
 
 
@@ -21,6 +22,7 @@ import { CuentagestionComponent } from './pages/cuentagestion/cuentagestion.comp
     AppComponent,
     ListadoPersonalComponent,
     CuentagestionComponent,
+    RegistrarPersonalComponent,
   ],
   providers: [],
   bootstrap: [AppComponent]
